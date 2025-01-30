8ca3512ecdcad98ffd21c447218a9fccfb5e3533
esysolana <esysolana@gmail.com>
2025-01-30 05:16:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿document.getElementById('buyButton').addEventListener('click', function () {
+    alert('Thank you for your interest in €$¥! Redirecting to the purchase page...');
+    // Здесь можно добавить ссылку на страницу покупки
+     window.location.href = 'https://pump.fun/coin/HzhdH8FTQSfNHeQQf1oueqwTzw7qSXxncPixmJnopump';
+});
