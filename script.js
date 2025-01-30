b63d37218f84e1a28c8ff82f44424f06e75a3e43
esysolana <esysolana@gmail.com>
2025-01-30 05:59:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 ﻿document.getElementById('buyButton').addEventListener('click', function () {
     alert('Thank you for your interest in €$¥! Redirecting to the purchase page...');
-    // Здесь можно добавить ссылку на страницу покупки
-     window.location.href = 'https://pump.fun/coin/HzhdH8FTQSfNHeQQf1oueqwTzw7qSXxncPixmJnopump';
+    window.location.href = 'https://pump.fun/coin/HzhdH8FTQSfNHeQQf1oueqwTzw7qSXxncPixmJnopump';
 });
+
+function copyLink() {
+    const link = "https://coinmarketcap.com/dexscan/solana/6JkKyjvc78mbDA8nr1JjzBCp2tEFZYtUEnaf9BpCAft9/";
+    navigator.clipboard.writeText(link).then(() => {
+        alert("Link copied to clipboard!");
+    });
+}
+
+function copyText() {
+    const text = "HzhdH8FTQSfNHeQQf1oueqwTzw7qSXxncPixmJnopump";
+    navigator.clipboard.writeText(text).then(() => {
+        alert("TOKEN copied to clipboard!");
+    });
+}
